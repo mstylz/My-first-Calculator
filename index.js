b10f68d40019f7f05815254ed37a05d94a375215
Mesut Yildiz <80417121+mstylz@users.noreply.github.com>
2025-06-09 16:51:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,62 @@
-//DOM Elements
+// DOM Elements
 const addTextToInput = function(digit) {
   let mainText = document.getElementById("mainText");
   mainText.value = mainText.value + digit;
 };
+
 const getValue = function() {
+  let mainText = document.getElementById("mainText");
   return +mainText.value;
 };
+
 const setValue = function(numericalValue) {
+  let mainText = document.getElementById("mainText");
   mainText.value = numericalValue.toString();
 };
-const changeSign = function() {
-  mainText.value = -mainText.value;
-};
+
 const clear1 = function() {
+  let mainText = document.getElementById("mainText");
   mainText.value = "";
 };
-var accumulator = 0;
-var operation = "";
-const minus = function() {
-  accumulator = getValue();
-  operation = "minus";
-  clear1();
-};
+
+let accumulator = 0;
+let operation = "";
+
+// Arithmetic operation setup functions
 const plus = function() {
   accumulator = getValue();
   operation = "plus";
   clear1();
 };
+
+const minus = function() {
+  accumulator = getValue();
+  operation = "minus";
+  clear1();
+};
+
+const multiply = function() {
+  accumulator = getValue();
+  operation = "multiply";
+  clear1();
+};
+
+// Perform calculation
 const equals = function() {
   switch (operation) {
-    case "minus":
-      setValue(accumulator - getValue());
-      break;
     case "plus":
       setValue(accumulator + getValue());
       break;
+    case "minus":
+      setValue(accumulator - getValue());
+      break;
+    case "multiply":
+      setValue(accumulator * getValue());
+      break;
   }
 };
+
+// Handle number/digit input
 const onClickNumber = function(digit) {
   addTextToInput(digit);
 };
-
